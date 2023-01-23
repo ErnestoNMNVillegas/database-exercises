e96fe83ecae1894d5431940ddb548bf72fdd2639
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-01-23 15:31:52 -0600

    database tables exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,8 +6,8 @@ CREATE TABLE albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                         artist VARCHAR(50) NOT NULL,
                         name  VARCHAR(50) NOT NULL,
-                        release_date  DATE NOT NULL,
-                        sales INT UNSIGNED NOT NULL,
+                        release_date  INT NOT NULL,
+                        sales FLOAT UNSIGNED NOT NULL,
                         genre VARCHAR(50) NOT NULL,
                         PRIMARY KEY (id)
 );
