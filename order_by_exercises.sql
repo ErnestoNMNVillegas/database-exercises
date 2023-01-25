2d7d43aa0b4df45ba04009eec1f465e850ff86c8
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-01-25 16:28:16 -0600

    order by exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,5 +1,6 @@
 use employees;
 
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
 
